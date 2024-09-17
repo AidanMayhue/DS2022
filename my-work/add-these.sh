7e5152eff04d16b9b7e88a3ce04d77927c352135
Aidan Mayhue <144569383+AidanMayhue@users.noreply.github.com>
2024-09-17 10:41:22 -0400

    adding scripts

---
+++ my-work/add-these.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "your first number is $1"
+echo "your second number is $2"
+echo "let me add those up ...."
+
+SUM=$(($1 + $2))
+
+sleep 3
+
+echo "your sum is: $SUM"
+
+
---
+++ my-work/convert-tsv-to-csv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#use tr to translate
+
+#
