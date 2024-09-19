8e75223fb67e24472b3518c357f241b5f2b9fb39
Aidan Mayhue <144569383+AidanMayhue@users.noreply.github.com>
2024-09-19 09:59:01 -0400

    help

---
+++ my-work/madlib.sh
@@ -0,0 +1,14 @@
+#1/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Name an unusual sport: " NOUN1
+read -p "2. Name an animal: " NOUN2
+read -p "3. Name a mode of transportation: " NOUN3
+read -p "4. Name a famous house: " NOUN4
+read -p "5. Name an instrument: " NOUN5
+read -p "6. Name a song: " NOUN6
+read -p "7. Name a hobby " NOUN7
+read -p "8. Name a type of bed " NOUN8
+
