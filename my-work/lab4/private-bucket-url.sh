b8e5fa330f65bbb36ff20e5c74ec86e9a1681b2b
Aidan Mayhue <144569383+AidanMayhue@users.noreply.github.com>
2024-10-02 11:38:40 -0400

    script 1 work now

--- my-work/lab4/private-bucket-url.sh
+++ my-work/lab4/private-bucket-url.sh
@@ -2,4 +2,4 @@
 
 curl https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png > google_logo.png
 aws s3 cp google_logo.png s3://ds2022-xdw9vp/
-aws s3 presign --expires in 604800 s3://ds2022-xdw9vp/google_logo.png
+aws s3 presign --expires-in 604800 s3://ds2022-xdw9vp/google_logo.png
